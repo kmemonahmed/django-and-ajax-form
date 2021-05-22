3aa9bca2f1aff32640c54750aacc4adc19c268ff
kmemon-ah <emon15-1205@diu.edu.bd>
2021-05-23 01:50:19 +0600

    broken fix

--- faceart_moc/settings.py
+++ faceart_moc/settings.py
@@ -119,5 +119,5 @@ import os
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT = os.path.join('BASE_DIR', 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
