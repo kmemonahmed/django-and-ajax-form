ca623f8ecccc8ab412b795a752d74f46ecb50cef
kmemon-ah <emon15-1205@diu.edu.bd>
2021-05-23 02:27:19 +0600

    fixing

--- faceart_moc/settings.py
+++ faceart_moc/settings.py
@@ -129,8 +129,8 @@ USE_TZ = True
 STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(PROJECT_ROOT, 'static'),
+# )
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
